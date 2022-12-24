5646d4124331cd84c5366bcbf6abba89a7a6c1c6
Saurabh Goyal <sgoyalsmvj@gmail.com>
2022-12-24 11:44:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <Gallery/>
+    <FindUs />
+    <Footer />
+  </div>
+);
+
+export default App;
